62a500e4e2d31074b201f9aed7afdcc35224b761
Andreas Zweili <andreas@2li.ch>
2020-10-12 22:26:21 +0200

    start to complete the main function

--- url2markdown/url2markdown.py
+++ url2markdown/url2markdown.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
+from cli import cli
+from downloader import downloader
+from file import write_to_file
+from header import Header
+
 
 def main():
-    pass
+    args = cli()
+    print(args.topics)
+    article = downloader(args.URL)
+    header = Header(article, args.topics)
+    write_to_file(article, header.build_header())
 
 
 if __name__ == "__main__":
