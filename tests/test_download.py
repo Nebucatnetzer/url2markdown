506297be6b55109b6c0c04fb46972fda393015b7
Andreas Zweili <andreas@2li.ch>
2020-10-12 15:17:05 +0200

    remove some tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -5,11 +5,3 @@ from url2markdown.downloader import downloader
 def test_get_content():
     article = downloader('https://newspaper.readthedocs.io/en/latest/user_guide/quickstart.html')
     assert type(article) == Article
-
-
-def test_convert_html_to_markdown():
-    assert False
-
-
-def test_add_topics():
-    assert False
