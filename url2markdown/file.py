ceb449e4774a343362fbadb0dfc2f890e6fee9c9
Andreas Zweili <andreas@2li.ch>
2020-10-13 21:18:04 +0200

    add a line between the header and the content

--- url2markdown/file.py
+++ url2markdown/file.py
@@ -5,4 +5,6 @@ def write_to_file(article, header):
     markdown = markdownify.markdownify(article.html, heading_style='ATX')
     with open(str(article.title) + ".md", 'w') as f:
         f.write(header)
+        f.write('------')
+        f.write('\n')
         f.write(markdown)
