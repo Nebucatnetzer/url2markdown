f2a13861f8dc479b29ea886c0ddee2bb3f7278d5
Andreas Zweili <andreas@2li.ch>
2020-10-13 22:15:35 +0200

    extend the helptext for topics

--- url2markdown/cli.py
+++ url2markdown/cli.py
@@ -7,5 +7,6 @@ def cli():
                         help="The URL of the article to convert to Markdown.",
                         type=str)
     parser.add_argument("--topics", help="A list of comma separated topics.")
+    parser.add_argument("--topics", help="A list of comma separated topics e.g. 'foo,bar'.")
     return parser.parse_args()
 
