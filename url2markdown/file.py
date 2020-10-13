5a6d0cefacb68f7d44f1baceb5c0d553c797ef3d
Andreas Zweili <andreas@2li.ch>
2020-10-13 21:17:31 +0200

    overwrite the file if it already exists

--- url2markdown/file.py
+++ url2markdown/file.py
@@ -2,7 +2,7 @@ import markdownify
 
 
 def write_to_file(article, header):
-    with open(str(article.title) + ".md", 'a') as f:
     markdown = markdownify.markdownify(article.html, heading_style='ATX')
+    with open(str(article.title) + ".md", 'w') as f:
         f.write(header)
         f.write(markdown)
