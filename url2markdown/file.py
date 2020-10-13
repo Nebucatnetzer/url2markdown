9b9aff27696a0e754f8244d66dad01ee81a95c20
Andreas Zweili <andreas@2li.ch>
2020-10-13 21:17:05 +0200

    use ATX style headers in markdown

--- url2markdown/file.py
+++ url2markdown/file.py
@@ -2,7 +2,7 @@ import markdownify
 
 
 def write_to_file(article, header):
-    markdown = markdownify.markdownify(article.html)
     with open(str(article.title) + ".md", 'a') as f:
+    markdown = markdownify.markdownify(article.html, heading_style='ATX')
         f.write(header)
         f.write(markdown)
