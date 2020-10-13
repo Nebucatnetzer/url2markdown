68782d1d220b5d48e6905b327fb697becc79b4cd
Andreas Zweili <andreas@2li.ch>
2020-10-13 22:15:49 +0200

    put the URL and file command in a mutual exclusive group

--- url2markdown/cli.py
+++ url2markdown/cli.py
@@ -3,10 +3,13 @@ import argparse
 
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument("URL",
-                        help="The URL of the article to convert to Markdown.",
-                        type=str)
-    parser.add_argument("--topics", help="A list of comma separated topics.")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--file',
+                       type=argparse.FileType('r'),
+                       help="A file containing one URL per line.")
+    group.add_argument('--url',
+                       help="The URL of the article to convert to Markdown.",
+                       type=str)
     parser.add_argument("--topics", help="A list of comma separated topics e.g. 'foo,bar'.")
     return parser.parse_args()
 
